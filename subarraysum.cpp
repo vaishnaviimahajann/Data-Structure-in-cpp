3c4be5d3ceca6e4fb60689ae1ced5b5917a233e2
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-01-11 22:13:38 +0530

    sumarray sum using kedane;s algorithm

--- subarraysum.cpp
+++ subarraysum.cpp
@@ -24,7 +24,7 @@ int main(){
     int n=6;
     printsubarraysum(arr,n);
     return 0;
-}*/
+}*//*
 #include<iostream>
 using namespace std;
 void printsubarraysum(int*arr,int n){
@@ -39,6 +39,28 @@ void printsubarraysum(int*arr,int n){
         cout<<endl;
      
     }
+     cout<<"maximum subarray sum ="<<maxsum<<endl;
+    
+}
+int main(){
+    int arr[6]={2,-3,6,-5,4,1};
+    int n=6;
+    printsubarraysum(arr,n);
+    return 0;
+}*/
+#include <iostream>
+using namespace std;
+void printsubarraysum(int*arr,int n){
+     int maxsum= INT8_MIN;
+     int cursum=0;
+     for(int i=0;i<n;i++){
+        cursum+=arr[i];
+        maxsum=max(maxsum,cursum);
+        if(cursum<0){
+            cursum=0;
+        }
+
+     }
      cout<<"maximum subarray sum ="<<maxsum<<endl;
     
 }
