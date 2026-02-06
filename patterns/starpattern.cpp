f7b4fda1e0fbeeb0269a2df7d3264988bf29a13d
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-06 20:06:00 +0530

    today i learn about patterns ..how each line and each condition work for pattern programs and also did some practice of it

---
+++ patterns/starpattern.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+   /* for(int i=1;i<=5;i++){
+
+         for(int j=1;j<=i;j++){
+            
+            cout<<"*";
+         }
+         cout<<endl;
+    }*/
+    
+   /* int n=5;
+    for(int i=5;i>=1;i--){
+
+         for(int j=1;j<=i;j++){
+            
+            cout<<"*";
+         }
+         cout<<endl;
+    }*/
+
+   /* for(int i=1;i<=5;i++){
+
+         for(int j=1;j<=i;j++){
+            
+            cout<<j;
+         }
+         cout<<endl;
+        }*/
+
+       /* for(int i=5;i>=1;i--){
+
+         for(int j=1;j<=i;j++){
+            
+            cout<<j;
+         }
+         cout<<endl;
+        }*/
+
+      /*  char ch='A';
+        for(int i=1;i<=5;i++){
+
+            for(int j=1;j<=i;j++)
+            {
+                cout<<ch;
+                ch++;
+            }
+            cout<<endl;
+        }*/
+
+          char ch='A';
+        for(int i=1;i<=5;i++){
+
+            for(int j=1;j<=i;j++)
+            {
+                cout<<ch;
+              
+            }
+              ch++;
+            cout<<endl;
+        }
+    return 0;
+}
