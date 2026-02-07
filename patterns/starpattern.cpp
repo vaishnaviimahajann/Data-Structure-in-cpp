889d1be823c329d264367de514b730717967a3d5
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-07 18:48:14 +0530

    did more practice of diamond pattern program , floyd's triangle hollow gram pattern

--- patterns/starpattern.cpp
+++ patterns/starpattern.cpp
@@ -50,7 +50,7 @@ int main(){
             cout<<endl;
         }*/
 
-          char ch='A';
+          /*char ch='A';
         for(int i=1;i<=5;i++){
 
             for(int j=1;j<=i;j++)
@@ -60,6 +60,74 @@ int main(){
             }
               ch++;
             cout<<endl;
+        }*/
+
+        /*int n=5;
+        for(int i=1;i<=n;i++){
+
+           cout<<"*";
+
+            for(int j=1;j<=n-1;j++){
+              if(i==1 || i==n){
+                cout<<"*";
+              }
+              else{
+                cout<<" ";
+              }
+            }
+            cout<<"*"<<endl;
+        }*/
+
+       /*int n=5;
+        for(int i=1;i<=n;i++){
+           for(int j=1;j<=n-i;j++){
+            cout<<" ";
+                  }
+           for(int j=1;j<=i;j++){
+            cout<<"*";
+           }
+           cout<<endl;
+           }*/
+
+        /*  int n =5;
+          int num=1;
+          for(int i=1;i<=n;i++){
+             for(int j=1;j<=i;j++){
+               cout<<num++<<" ";
+             }
+            
+             cout<<endl;
+          }*/
+
+          /*int n =5;
+          int num=15;
+          for(int i=5;i>=1;i--){
+             for(int j=1;j<=i;j++){
+               cout<<num--<<" ";
+             }
+            
+             cout<<endl;
+          }*/
+
+          int n=5;
+          for(int i=1;i<=n;i++){
+             for(int j=1;j<=n-i;j++){
+              cout<< " ";
+             }
+              for(int j=1;j<=2*i-1;j++){
+              cout<<"*";
+             }
+             cout<<endl;
+          }
+          for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+              cout<<" ";
+            }
+            for(int j=1;j<=2*i-1;j++){
+              cout<<"*";
+            }
+            cout<<endl;
+          }
+             return 0;
         }
-    return 0;
-}
+  
