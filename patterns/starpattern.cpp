9e22ad455191b986a73b4297fa4fa8c485f8a6a6
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-08 18:11:19 +0530

    create diamond star pattern using loops

--- patterns/starpattern.cpp
+++ patterns/starpattern.cpp
@@ -109,7 +109,7 @@ int main(){
              cout<<endl;
           }*/
 
-          int n=5;
+         /* int n=5;
           for(int i=1;i<=n;i++){
              for(int j=1;j<=n-i;j++){
               cout<< " ";
@@ -127,6 +127,32 @@ int main(){
               cout<<"*";
             }
             cout<<endl;
+          }*/
+
+           int n=5;
+          for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                  cout<<"*";
+                }
+                for(int j=1;j<=(2*n-i);j++){
+                  cout<<"  ";
+                }
+                 for(int j=1;j<=i;j++){
+                  cout<<"*";
+                }
+              cout<<endl;
+          }
+           for(int i=n-1;i>=1;i--){
+                for(int j=1;j<=i;j++){
+                  cout<<"*";
+                }
+                for(int j=1;j<=(2*n-i);j++){
+                  cout<<"  ";
+                }
+                 for(int j=1;j<=i;j++){
+                  cout<<"*";
+                }
+              cout<<endl;
           }
              return 0;
         }
