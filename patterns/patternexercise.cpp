2c0b9086c1d3cc61746b3127d186a915e8fa9475
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-08 18:12:52 +0530

    Implement rhombus and triangle pattern problems in C++

---
+++ patterns/patternexercise.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int n=4;
+    for(int i=0;i<=n;i++){
+             for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    cout<<1;
+                }else{
+                    cout<<0;
+                }
+             }
+             cout<<endl;
+
+    }
+    return 0;
+}*/
+
+int n=5;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    for(int j=1;j<=n;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+    
+}return 0;
+}
