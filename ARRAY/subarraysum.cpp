e874e6fcf90f38055bab0628453f286a0a804471
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-03-03 18:58:42 +0530

    finds subarray sum using 3 approches

--- ARRAY/subarraysum.cpp
+++ ARRAY/subarraysum.cpp
@@ -24,49 +24,51 @@ int main(){
     int n=6;
     printsubarraysum(arr,n);
     return 0;
-}*//*
+}*/
+/*
+USING OPTIMIZED BRUTE FORCE TIIME COMPLEXITY BIG O N SQUARE
 #include<iostream>
+#include <climits>
 using namespace std;
 void printsubarraysum(int*arr,int n){
-     int maxsum= INT8_MIN;
+     int maxsum= INT_MIN;
     for(int start=0;start<n;start++){
-       
+             int cursum=0;
         for(int end=start;end<n;end++){
-            int cursum=0;
-            cursum+=arr[end];
+              cursum+=arr[end];
             maxsum=max(maxsum,cursum);
         }
-        cout<<endl;
-     
     }
      cout<<"maximum subarray sum ="<<maxsum<<endl;
     
 }
 int main(){
     int arr[6]={2,-3,6,-5,4,1};
-    int n=6;
+    int n=sizeof(arr)/sizeof(int);
     printsubarraysum(arr,n);
     return 0;
-}*/
-#include <iostream>
+}
+*/
+#include<iostream>
+#include <climits>
 using namespace std;
 void printsubarraysum(int*arr,int n){
-     int maxsum= INT8_MIN;
+     int maxsum= INT_MIN;
      int cursum=0;
-     for(int i=0;i<n;i++){
-        cursum+=arr[i];
-        maxsum=max(maxsum,cursum);
-        if(cursum<0){
+    for(int i=0;i<n;i++){
+           cursum+=arr[i];
+           maxsum=max(maxsum,cursum); 
+           if(cursum<0){
             cursum=0;
-        }
-
-     }
-     cout<<"maximum subarray sum ="<<maxsum<<endl;
+           }
+    }
+        cout<<"maximum subarray sum ="<<maxsum<<endl;
     
 }
 int main(){
     int arr[6]={2,-3,6,-5,4,1};
-    int n=6;
+    int n=sizeof(arr)/sizeof(int);
     printsubarraysum(arr,n);
     return 0;
 }
+
