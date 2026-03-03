5f64a1dca6d6123ac7543bb5634a32a3a1341e5c
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-03-03 18:57:58 +0530

    revised subarray sum logic and also learn how to find subarray for array

--- ARRAY/subarray.cpp
+++ ARRAY/subarray.cpp
@@ -1,19 +1,57 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
+//here we are printing the starting and ending index of the subarray
 void printsubarray(int *arr,int n){
     for(int start=0;start<n;start++){
         for(int end=start;end<n;end++){
-           // cout<<"("<< start<<","<< end<<")";
-           for(int i=start;i<end;i++){
-              cout<<arr[i];
-           }
-
-           cout<<",";
+           cout<<"("<< start<<","<< end<<")";
         }
         cout<<endl;
     }
 }
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5;
+    printsubarray(arr,n);
+    return 0;
+}*//*
+#include<iostream>
+using namespace std;
 
+ // here we are printing an subarray of array
+
+void printsubarray(int *arr,int n){
+    for(int start=0;start<n;start++){
+          for(int end=start;end<n;end++){
+                   for(int i=start;i<=end;i++){
+                              cout<<arr[i];
+                        }
+                cout<<",";
+         }
+        cout<<endl;
+    }
+}
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5;
+    printsubarray(arr,n);
+    return 0;
+}*/
+#include<iostream>
+using namespace std;
+
+ // here we are printing an subarray of array
+
+void printsubarray(int *arr,int n){
+    for(int start=0;start<n;start++){
+        int sum=0;
+          for(int end=start;end<n;end++){
+                   sum+=arr[end];
+                     cout<<sum<<",";
+         }
+        cout<<endl;
+    }
+}
 int main(){
     int arr[5]={1,2,3,4,5};
     int n=5;
