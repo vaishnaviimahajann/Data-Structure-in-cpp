7c7ca7587fc3fb7df2c25ddaee259dacf863c720
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-03-04 19:28:07 +0530

    revised watertrap leetcode based example

--- ARRAY/watertrap.cpp
+++ ARRAY/watertrap.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <algorithm>
 using namespace std;
 
@@ -42,4 +42,54 @@ int main() {
 
     trap(height, n);
     return 0;
+}*/
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+void trap(int *height, int n) {
+    int leftmax[200000];
+    int rightmax[200000];
+    leftmax[0] = INT_MIN;
+    rightmax[n - 1] = INT_MIN;
+
+    // LEFT MAX
+    leftmax[0] = height[0];
+    for (int i = 1; i < n; i++) {
+        leftmax[i] = max(leftmax[i - 1], height[i-1]);
+    }
+
+    // RIGHT MAX
+    rightmax[n - 1] = height[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+        rightmax[i] = max(rightmax[i + 1], height[i+1]);
+    }
+
+     cout << "Leftmax: ";
+    for (int i = 0; i < n; i++) cout << leftmax[i] << " ";
+
+    cout << "\nRightmax: ";
+    for (int i = 0; i < n; i++) cout << rightmax[i] << " ";
+
+    int watertrap = 0;
+
+    for (int i = 1; i < n - 1; i++) {
+        int currwater = min(leftmax[i], rightmax[i]) - height[i];
+        if (currwater > 0) {
+            watertrap += currwater;
+        }
+    }
+
+    cout << "Water trapped = " << watertrap << endl;
 }
+
+int main() {
+    int height[7] = {4, 2, 0, 6, 3, 2, 5};
+    int n = sizeof(height) / sizeof(int);
+
+    trap(height, n);
+    return 0;
+}
+
