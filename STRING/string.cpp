2f2b9fcfdd8064edcfc6c99572e6c4ffa277846b
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-03-09 00:32:25 +0530

    string built-in functions: concatnation,substr,strlen,strtoupper-lower

--- STRING/string.cpp
+++ STRING/string.cpp
@@ -6,4 +6,22 @@ int main() {
     string s = "Hello";
     cout << s[0];      // H
     cout << s.length(); // 5
+
+   string  str1="hieee , there!";
+    string str2="hieee , there!";
+    if(str1==str2){
+        cout<<"same string";
+    }
+    else{
+        cout<<"different string";
+    }
+
+    cout << s.substr(0, 3); // Hello
+    cout << s.substr(2);    // llo
+    cout<<"size of string: "<<str1.length()<<endl; // 5
+    cout<<"size of string: "<<str1.size()<<endl; // 5
+
+    //concatination
+    string str3 = str1 + " " + str2;
+    cout<<str3<<endl;
 }
