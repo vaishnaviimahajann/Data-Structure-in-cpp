b60a40a90b9cd3cf1adbca70ea9aa40494457809
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-06 07:36:45 +0530

    learn how i can insert data at any pos

--- linkedlist/doublylinkedlist.cpp
+++ linkedlist/doublylinkedlist.cpp
@@ -33,6 +33,40 @@ class list{
         }
    }
 
+   void push_back(int val){
+    node* newnode=new node(val);
+    if(head==NULL){
+        head=tail=newnode;
+    }
+    else{
+        tail->next=newnode;
+        newnode->prev=tail;
+        tail=newnode;
+     }
+ }
+
+  void insert_middle(int val,int pos){
+    node *newnode=new node(val);
+    node* temp=head;
+
+    for(int i=1;i<pos-1;i++){
+        if(temp==NULL){
+            cout<<"position is invalid"<<endl;
+            return;
+        }
+        temp=temp->next;
+    }
+    //temp is now at pos-1 i.e the node after which we want to insert the new node
+    newnode->next=temp->next;
+    newnode->prev=temp;
+
+    if(temp->next!=NULL){
+        temp->next->prev=newnode;
+    }
+    temp->next=newnode;
+  }
+   
+
    void printList(){
         node* temp = head;
         while(temp != NULL){
@@ -63,8 +97,8 @@ int main(){
         ll.push_front(1);
         ll.push_front(2);
         ll.push_front(3);
-       
         ll.printList();
-        ll.printList2();
+        ll.insert_middle(10, 2);
+        ll.printList();
     return 0;
 }
