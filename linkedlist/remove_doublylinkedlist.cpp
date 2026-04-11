614f022b4aefc7353b3820e1152f7351b4fc36cd
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-11 23:45:25 +0530

    deleting front front and end of doubly linkedlist

---
+++ linkedlist/remove_doublylinkedlist.cpp
@@ -0,0 +1,155 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node *next;
+    node *prev;
+
+    node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }
+};
+
+class list{
+    node *head;
+    node *tail;
+    public:
+    list(){
+        head=NULL;
+        tail=NULL;
+    }
+
+    void push_front(int val){
+        node *newnode=new node(val);
+        if(head==NULL){
+            head=tail=newnode;
+        }else{
+            newnode->next=head;
+            head->prev=newnode;
+            head=newnode;
+        }
+   }
+
+   void push_back(int val){
+    node* newnode=new node(val);
+    if(head==NULL){
+        head=tail=newnode;
+    }
+    else{
+        tail->next=newnode;
+        newnode->prev=tail;
+        tail=newnode;
+     }
+ }
+
+  void insert_middle(int val,int pos){
+    node *newnode=new node(val);
+    node* temp=head;
+
+    for(int i=1;i<pos-1;i++){
+        if(temp==NULL){
+            cout<<"position is invalid"<<endl;
+            return;
+        }
+        temp=temp->next;
+    }
+    //temp is now at pos-1 i.e the node after which we want to insert the new node
+    newnode->next=temp->next;
+    newnode->prev=temp;
+
+    if(temp->next!=NULL){
+        temp->next->prev=newnode;
+    }
+    temp->next=newnode;
+  }
+
+  //itreative search on linkedlist
+  int search(int key){
+    node* temp=head;
+    int index=0;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return index;
+        }
+        temp=temp->next;
+        index++;
+    }
+    return -1; //key not found
+  }
+   
+
+   void printList(){
+        node* temp = head;
+        while(temp != NULL){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+     void printList2(){
+        node* temp = tail;
+        while(temp != NULL){
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
+        cout << endl;
+    }
+
+    void pop_front(){
+        if(head == NULL){
+            cout<<"List is empty. Cannot pop from front."<<endl;
+            return;
+        }
+        node* temp = head;
+        head = head->next;
+        if(head != NULL){
+            head->prev = NULL; // Detach the node
+        } else {
+            tail = NULL; // List is now empty
+        }
+        temp->next = NULL; // Detach the node
+        delete temp; // Free memory
+    }
+
+    void  pop_back(){
+        if(tail == NULL){
+            cout<<"List is empty. Cannot pop from back."<<endl;
+            return;
+        }
+        node* temp = tail;
+        tail = tail->prev;
+        if(tail != NULL){
+            tail->next = NULL; // Detach the node
+        } else {
+            head = NULL; // List is now empty
+        }
+        temp->prev = NULL; // Detach the node
+        delete temp; // Free memory
+    }
+
+      
+};
+
+
+
+int main(){
+
+
+        list ll;
+        ll.push_front(1);
+        ll.push_front(2);
+        ll.push_front(3);
+        ll.printList();
+        ll.insert_middle(10, 2);
+        ll.printList();
+        cout << "Index of 10: " << ll.search(3) << endl;
+        ll.pop_front();
+        ll.printList();
+        ll.pop_back();
+        ll.printList();
+    return 0;
+}
