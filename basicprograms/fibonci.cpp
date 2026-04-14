dad80ee4c36f826bb86fde593629fe3c646d8f91
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-14 16:19:38 +0530

    fibonci series

---
+++ basicprograms/fibonci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of terms: ";
+    cin>>n;
+
+    int a = 0, b = 1, c;
+
+    for(int i = 1; i <= n; i++){
+        cout<<a<<" ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
