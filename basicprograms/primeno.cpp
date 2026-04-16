1df2cc65b0f06c885138c684375030276dab861b
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-16 14:38:41 +0530

    second logic for prime no

--- basicprograms/primeno.cpp
+++ basicprograms/primeno.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 int main(){
@@ -12,5 +12,30 @@ int main(){
     }else{
         cout<<"NOT PRIME";
     }
+    return 0;
+}*/
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"ENTER NO"<<endl;
+    cin>>n;
+
+    int count = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+
+    if(count == 2){
+        cout<<"PRIME";
+    }else{
+        cout<<"NOT PRIME";
+    }
+
     return 0;
 }
