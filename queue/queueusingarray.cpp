3da2c3ce4b0bfa8d98582720131d6c6b0d70abc3
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-05-07 00:15:34 +0530

    queue using array is basically circular queue implementation in C++

---
+++ queue/queueusingarray.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+            int* arr;
+
+            int capacity;
+            int currsize;
+
+            int f,r;
+        public:
+        Queue(int capacity){
+             this->capacity= capacity;
+             arr = new int[capacity];
+             currsize=0;
+
+             f = 0;
+             r =  -1;
+        }
+        void push(int data){
+
+            if(currsize == capacity){
+                cout<<"Queue is full"<<endl;
+                return;
+            }
+
+               r = (r+1)%capacity;
+               arr[r]=data;
+                currsize++;
+               
+        }
+        void pop(){
+            if(currsize == 0){
+                cout<<"Queue is empty"<<endl;
+                return;
+            }
+
+        f = (f + 1) % capacity;
+        currsize--;
+
+        }
+        int front(){
+            if(currsize == 0){
+                cout<<"Queue is empty"<<endl;
+                return -1;
+            }
+            return arr[f];
+        }
+        bool empty(){
+            return currsize == 0;
+        }
+};
+
+int main(){
+    Queue q(4);
+    q.push(1);
+    q.push(2);
+
+    q.push(3);
+    q.push(4);
+    while(!q.empty()){
+        cout<<"Front element: "<<q.front()<<endl;
+        q.pop();
+    }
+
+    cout<<"Front element: "<<q.front()<<endl;
+    q.pop();
+    cout<<"Front element after pop: "<<q.front()<<endl;
+    cout<<"Is queue empty? "<<q.empty()<<endl;
+
+    return 0;
+      
+}
+
+//git commit -m "queue using array is basically circular queue implementation in C++"
