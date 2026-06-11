0b85a1a8c8e4207a431028f1cb5e48bc652dfcbb
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-06-11 11:29:40 +0530

    Array Rotation is the process of shifting elements of array circulary to the leeft or right side

---
+++ problems/array/3_leftrotateby1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void leftrotateby1(int arr[] , int start , int end){
+    while(start < end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    
+}
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int d = 1;
+    leftrotateby1(arr,0,d-1);
+    leftrotateby1(arr,d,n-1);
+    leftrotateby1(arr,0,n-1);
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+
+    return 0;
+}
