5802e1d2a4d638bdb701c250a75c77c2e3a78185
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-06-24 10:55:11 +0530

    tried optimal solution forr two sum probkem using hash map

--- problems/array/11_twosum.cpp
+++ problems/array/11_twosum.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 using namespace std;
 
@@ -21,6 +21,37 @@ int main(){
     
     vector<int> ans = twoSum(arr, target);
     
+    cout << "Indexes: [" << ans[0] << ", " << ans[1] << "]" << endl;
+    
+    return 0;
+}*/
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& arr, int target){
+    unordered_map<int,int> mp;
+    int n = arr.size();
+    
+    for(int i = 0; i < n; i++){
+        int compliment = target - arr[i];
+        
+        if(mp.find(compliment) != mp.end()){
+            return {mp[compliment], i};
+        }
+        
+        mp[arr[i]] = i; 
+    }
+    
+    return {-1, -1};
+}
+
+int main(){
+    vector<int> arr = {2, 7, 11, 15, 22, 23};
+    int target = 45;
+    
+    vector<int> ans = twoSum(arr, target);
     cout << "Indexes: [" << ans[0] << ", " << ans[1] << "]" << endl;
     
     return 0;
