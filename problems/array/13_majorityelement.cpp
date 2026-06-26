81da2746435550d55d31b3da55d6dc158be9f169
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-06-26 10:46:07 +0530

    tried better approch for this  majority element

--- problems/array/13_majorityelement.cpp
+++ problems/array/13_majorityelement.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 using namespace std;
 
@@ -23,6 +23,37 @@ int majorityElement(vector<int>& nums){
 
 int main(){
     vector<int> nums = {3, 2, 3};
+    
+    cout << "Majority Element: " << majorityElement(nums) << endl;
+    
+    return 0;
+}*/
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int majorityElement(vector<int>& nums){
+    int n = nums.size();
+    unordered_map<int,int> mp;
+    
+    // Step 1 — frequency count
+    for(int i = 0; i < n; i++){
+        mp[nums[i]]++;
+    }
+    
+    // Step 2 — majority dhundho
+    for(auto it : mp){
+        if(it.second > n/2){
+            return it.first;
+        }
+    }
+    
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
     
     cout << "Majority Element: " << majorityElement(nums) << endl;
     
