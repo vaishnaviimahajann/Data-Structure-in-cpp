71c40ad936fb84d915e44112f730a1054b23c805
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-06-28 11:15:07 +0530

    optimal approch  for printing subarray sum array

--- problems/array/15_maxsubarraay.cpp
+++ problems/array/15_maxsubarraay.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 #include<climits>
 using namespace std;
@@ -27,8 +27,47 @@ void printSubarray(vector<int>& nums){
     cout << endl;
 }
 
+
 int main(){
     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     printSubarray(nums);
+    return 0;
+}*/
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+void printSubarrayOptimal(vector<int>& nums){
+    int n = nums.size();
+    int maxsum = INT_MIN, currsum = 0;
+    int start = 0, end = 0, tempStart = 0;
+    
+    for(int i = 0; i < n; i++){
+        currsum += nums[i];
+        
+        if(currsum > maxsum){
+            maxsum = currsum;
+            start = tempStart;  // naya start!
+            end = i;            // current end!
+        }
+        
+        if(currsum < 0){
+            currsum = 0;
+            tempStart = i + 1;  // aage se fresh start!
+        }
+    }
+    
+    cout << "Max Sum: " << maxsum << endl;
+    cout << "Subarray: ";
+    for(int i = start; i <= end; i++){
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    printSubarrayOptimal(nums);
     return 0;
 }
