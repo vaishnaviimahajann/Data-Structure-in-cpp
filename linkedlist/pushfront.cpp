16a320365837a38340b65c499eb91d7a91e0ef86
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-09-16 18:43:34 +0530

    learn how to add /  push in the linkelist

---
+++ linkedlist/pushfront.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+
+    }
+};
+class list{
+public:
+    node*head;
+    node*tail;
+
+    list(){
+        head=NULL;
+        tail=NULL;
+    }
+
+void push_front(int val){
+    node* newnode = new node(val);
+    if(head==NULL){
+        head=tail=newnode;
+    }
+    else{
+        newnode->next=head;
+        head=newnode;
+    }
+}
+void showll(){
+    node* temp = head;
+    while(head!=NULL){
+        cout<< temp->data<<" ";
+        temp = temp->next;
+    }
+}
+};
+int main(){
+    list l;
+    l.push_front(10);
+    l.push_front(20);
+    l.push_front(30);
+    l.showll();
+    return 0;
+}
