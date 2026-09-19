498ce775ff7979e852f62c56dbd9eca42a1a3db0
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-09-19 09:16:50 +0530

    learn how to push at front middle and end in doubly ll

---
+++ linkedlist/doublyll.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+class node{
+    public :
+    int data;
+    node* next;
+    node* prev;
+
+    node(int val){
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+
+};
+class list{
+
+    node* head;
+    node*tail;
+  
+    public:
+    list(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val){
+        node* newnode = new node(val);
+        if(head== NULL){
+            head = tail = newnode;
+        }
+        else{
+            newnode -> next = head;
+            head->prev = newnode;
+            head = newnode;
+        }
+    }
+ 
+    void push_back(int val){
+        node* newnode = new node(val);
+        if(head== NULL){
+            head = tail = newnode;
+        }else{
+            tail->next = newnode;
+            newnode->prev = tail;
+            tail = newnode;
+        }
+    }
+    void showll(){
+        node* temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+
+};
+int main(){
+    
+    list l;
+    l.push_front(10);
+    l.push_front(20);
+    l.push_front(30);
+
+
+    l.showll();
+    cout<<endl;
+    l.push_back(40);
+    l.push_back(50);
+    l.showll();
+}
